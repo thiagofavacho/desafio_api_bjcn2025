5cc950f0745ce9c5df4f22f1e6747efa8311bcd9
Thiago Favacho R. <thiagofr89@hotmail.com>
2025-03-06 22:32:52 -0300

    Initial commit

---
+++ src/main/java/api_bjcn2025/DesafioApiBjcn2025Application.java
@@ -0,0 +1,13 @@
+package api_bjcn2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiBjcn2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiBjcn2025Application.class, args);
+	}
+
+}
